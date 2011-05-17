8fbe0344d55dc3a8507b97025d41e7119da78869
Lincoln Simmons <lincolnsimmons@gmail.com>
2011-05-16 23:18:15 -0500

    Modified Makefiles and extracted files for viewing in hoppity and meepmeep puzzles

---
+++ meepmeep/meepmeep.c
@@ -0,0 +1,12 @@
+/* Solution to the "Meep meep!" Facebook puzzle
+ * By: Lincoln Simmons
+ * Date: March 3, 2011
+ */
+
+#include <stdio.h>
+
+int main (void)
+{
+	printf("Meep meep!\n");
+	return 0;
+}
